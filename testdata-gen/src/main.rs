646e282f0dfb4e4a849017fee5928f8ef52f07a4
Dominik Harz <dominik@interlay.io>
2020-09-22 21:50:13 +0200

    add vault registration

--- testdata-gen/src/main.rs
+++ testdata-gen/src/main.rs
@@ -1,3 +1,4 @@
+use std::str::FromStr;
 use sp_keyring::AccountKeyring;
 use primitive_types::{H256, U256};
 use sp_core::H160;
@@ -11,6 +12,8 @@ use hex;
 
 const POLKA_BTC_URL: &str = "ws://127.0.0.1:9944";
 const ALICE_BTC_ADDRESS: &str = "66c7060feb882664ae62ffad0051fe843e318e85";
+const BOB_BTC_ADDRESS: &str = "BF3408F6C0DEC0879F7C1D4D0A5E8813FC0DB569";
+const VAULT_COLLATERAL: u128 = 100;
 
 /// Generates testdata to be used on a development environment of the BTC-Parachain
 #[tokio::main]
@@ -26,7 +29,8 @@ async fn main() -> Result<(), Error> {
 
     // set exchange rate to 0.00038 at granularity 5
     let btc_to_dot_rate: u128 = 38;
-    oracle_prov.set_exchange_rate_info(btc_to_dot_rate).await?;
+    // TODO: Error: XtError(Metadata(ErrorNotFound(0)))
+    // oracle_prov.set_exchange_rate_info(btc_to_dot_rate).await?;
 
     // get exchange rate
     let (rate, _time, _delay) = oracle_prov.get_exchange_rate_info().await?;
@@ -35,12 +39,12 @@ async fn main() -> Result<(), Error> {
     // BTC RELAY
     let relay_prov = alice_prov.clone();
     
-    let dest_address = H160::from_slice(
+    let alice_address = H160::from_slice(
         hex::decode(ALICE_BTC_ADDRESS)
                 .unwrap()
                 .as_slice(),
         ); 
-    let address = Address::from(*dest_address.as_fixed_bytes()); 
+    let address = Address::from(*alice_address.as_fixed_bytes()); 
     let mut height: u32 = 1;
     // initialize BTC Relay with one block
     let init_block = BlockBuilder::new()
@@ -53,7 +57,16 @@ async fn main() -> Result<(), Error> {
     let raw_init_block_header = RawBlockHeader::from_bytes(&init_block.header.format())
         .expect("could not serialize block header");
 
-    relay_prov.initialize_btc_relay(raw_init_block_header, height).await?;
+    // relay_prov.initialize_btc_relay(raw_init_block_header, height).await?;
+
+    // VAULT REGISTRY
+    let vault_prov = bob_prov.clone();
+    let bob_address = H160::from_slice(
+        hex::decode(BOB_BTC_ADDRESS)
+                .unwrap()
+                .as_slice(),
+        ); 
+    vault_prov.register_vault(VAULT_COLLATERAL, bob_address).await?;
 
     Ok(())
 }
