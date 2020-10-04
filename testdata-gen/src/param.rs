dc0d33f5571be727c97266eb85e2da218e66c94e
Dominik Harz <dominik@interlay.io>
2020-10-04 21:27:45 +0200

    update testdata

--- testdata-gen/src/param.rs
+++ testdata-gen/src/param.rs
@@ -1,8 +1,10 @@
 #![allow(dead_code)]
 pub const POLKA_BTC_URL: &str = "ws://127.0.0.1:9944";
-pub const ALICE_BTC_ADDRESS: &str = "66c7060feb882664ae62ffad0051fe843e318e85";
-pub const BOB_BTC_ADDRESS: &str = "bf3408f6c0dec0879f7c1d4d0a5e8813fc0db569";
-pub const BOB_VAULT_COLLATERAL: u128 = 1_000_000;
+pub const ALICE_BTC_ADDRESS: &str = "tb1qmwv7aqktv5l44x65qmsk6u4z9wh66nazv9rgv3"; // Alexei
+pub const BOB_BTC_ADDRESS: &str = "tb1q4kspwcf42cqp66hrhw407djna4dgpw9lsnfx5e"; // Dom
+pub const BOB_VAULT_COLLATERAL: u128 = 100_000_000_000;
+pub const CHARLIE_BTC_ADDRESS: &str = "tb1q4kspwcf42cqp66hrhw407djna4dgpw9lsnfx5e"; // Dom
+pub const CHARLIE_VAULT_COLLATERAL: u128 = 100_000_000_000;
 pub const GRIEFING_COLLATERAL: u128 = 100;
 pub const ALICE_ISSUE_AMOUNT: u128 = 100_000;
 pub const ALICE_REDEEM_AMOUNT_1: u128 = 500;
--- testdata-gen/src/utils.rs
+++ testdata-gen/src/utils.rs
@@ -1,6 +1,11 @@
 #![allow(dead_code)]
 use sp_core::H160;
+use std::str::FromStr;
 
 pub fn get_address_from_string(btc_address: &str) -> H160 {
+    H160::from_str(btc_address).unwrap()
+}
+
+pub fn get_address_from_hex(btc_address: &str) -> H160 {
     H160::from_slice(hex::decode(btc_address).unwrap().as_slice())
 }
