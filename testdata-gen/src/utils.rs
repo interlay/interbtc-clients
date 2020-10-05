5dd4488ef447034d1181ca5c031fa1dc83050ea8
Dominik Harz <dominik@interlay.io>
2020-10-05 11:14:39 +0200

    btc address conversion

--- testdata-gen/src/utils.rs
+++ testdata-gen/src/utils.rs
@@ -1,9 +1,17 @@
 #![allow(dead_code)]
 use sp_core::H160;
+use relayer_core::bitcoin::bitcoincore_rpc::bitcoin::{hashes::Hash, util::address::{ Payload, Address }};
 use std::str::FromStr;
 
+
 pub fn get_address_from_string(btc_address: &str) -> H160 {
-    H160::from_str(btc_address).unwrap()
+    let addr = Address::from_str(btc_address).unwrap();
+    let hash = match addr.payload {
+        Payload::PubkeyHash(hash) => hash.as_hash().into_inner(),
+        Payload::ScriptHash(hash) => hash.as_hash().into_inner(),
+        Payload::WitnessProgram { version, program } => {} 
+    };
+    H160::from_slice(hash)
 }
 
 pub fn get_address_from_hex(btc_address: &str) -> H160 {
