02b70905ece75400b3f15ba0fe06c134828c1189
Sander Bosma <sanderbosma@gmail.com>
2020-10-19 19:20:08 +0200

    testdata-gen: update bitcoin code & test case for vault redeem

--- testdata-gen/src/param.rs
+++ testdata-gen/src/param.rs
@@ -2,6 +2,8 @@
 pub const POLKA_BTC_URL: &str = "ws://127.0.0.1:9944";
 pub const ALICE_BTC_ADDRESS: &str = "tb1qmwv7aqktv5l44x65qmsk6u4z9wh66nazv9rgv3"; // Alexei
 pub const BOB_BTC_ADDRESS: &str = "tb1qxmesg85g43h4rc8vpye24gysdqz2vx6cjf2n3q"; // Dom
+                                                                                // pub const ALICE_BTC_ADDRESS: &str = "bcrt1q6v2c7q7uv8vu6xle2k9ryfj3y3fuuy4rqnl50f"; // Sander2
+                                                                                // pub const BOB_BTC_ADDRESS: &str = "bcrt1qywv76cslwf9puhaz0yzxcnpcs96gz0g24fzd5r"; // Sander
 pub const BOB_VAULT_COLLATERAL: u128 = 1_000_000_000_000;
 pub const CHARLIE_BTC_ADDRESS: &str = "tb1qznj9x08zqre3rn7njwxppaj5t2fm373d6k75v7"; // Dom
 pub const CHARLIE_VAULT_COLLATERAL: u128 = 5_000_000_000_000;
