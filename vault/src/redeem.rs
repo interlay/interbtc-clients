326b8a0ffcedcc5ea2a947641c3a5c4a99bcd0b9
Sander Bosma <sanderbosma@gmail.com>
2020-11-19 11:21:01 +0100

    feat(vault): log btc amounts

--- vault/src/redeem.rs
+++ vault/src/redeem.rs
@@ -28,7 +28,7 @@ pub async fn listen_for_redeem_requests(
                 if event.vault_id != vault_id.clone() {
                     return;
                 }
-                info!("Received redeem request #{}", event.redeem_id);
+                info!("Received redeem request #{} for amount {}", event.redeem_id, event.amount_polka_btc);
 
                 // within this event callback, we captured the arguments of listen_for_redeem_requests
                 // by reference. Since spawn requires static lifetimes, we will need to capture the
@@ -44,7 +44,7 @@ pub async fn listen_for_redeem_requests(
                         .await;
 
                     match result {
-                        Ok(_) => info!("Completed redeem request #{}", event.redeem_id),
+                        Ok(_) => info!("Completed redeem request #{} with amount {}", event.redeem_id, event.amount_polka_btc),
                         Err(e) => error!(
                             "Failed to process redeem request #{}: {}",
                             event.redeem_id,
