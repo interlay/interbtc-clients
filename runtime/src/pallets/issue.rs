b56b3da0523e206a8b143ae8f07a993843fba5c8
Sander Bosma <sanderbosma@gmail.com>
2021-02-15 17:45:49 +0100

    chore: updated dependencies

--- runtime/src/pallets/issue.rs
+++ runtime/src/pallets/issue.rs
@@ -1,58 +1,14 @@
 use super::{Core, CoreEventsDecoder};
-use crate::{BtcAddress, BtcPublicKey};
 use core::marker::PhantomData;
 pub use module_bitcoin::types::H256Le;
+pub use module_issue::IssueRequest;
 use parity_scale_codec::{Decode, Encode};
-use serde::{Deserialize, Deserializer, Serialize, Serializer};
+use serde::Serialize;
 use std::fmt::Debug;
 use substrate_subxt_proc_macro::{module, Call, Event, Store};
 
 #[module]
 pub trait Issue: Core {}
-
-// TODO: use the type exported by module_issue when dependency conflicts are resolved.
-// Due to a known bug in serde we need to specify how u128 is (de)serialized.
-// See https://github.com/paritytech/substrate/issues/4641
-#[derive(Encode, Decode, Default, Clone, PartialEq, Debug, Serialize, Deserialize)]
-pub struct IssueRequest<AccountId, BlockNumber, PolkaBTC, DOT> {
-    pub vault: AccountId,
-    pub opentime: BlockNumber,
-    #[serde(bound(deserialize = "DOT: std::str::FromStr"))]
-    #[serde(deserialize_with = "deserialize_from_string")]
-    #[serde(bound(serialize = "DOT: std::fmt::Display"))]
-    #[serde(serialize_with = "serialize_as_string")]
-    pub griefing_collateral: DOT,
-    #[serde(bound(deserialize = "PolkaBTC: std::str::FromStr"))]
-    #[serde(deserialize_with = "deserialize_from_string")]
-    #[serde(bound(serialize = "PolkaBTC: std::fmt::Display"))]
-    #[serde(serialize_with = "serialize_as_string")]
-    pub amount: PolkaBTC,
-    #[serde(bound(deserialize = "PolkaBTC: std::str::FromStr"))]
-    #[serde(deserialize_with = "deserialize_from_string")]
-    #[serde(bound(serialize = "PolkaBTC: std::fmt::Display"))]
-    #[serde(serialize_with = "serialize_as_string")]
-    pub fee: PolkaBTC,
-    pub requester: AccountId,
-    pub btc_address: BtcAddress,
-    pub btc_public_key: BtcPublicKey,
-    pub completed: bool,
-    pub cancelled: bool,
-}
-
-fn serialize_as_string<S: Serializer, T: std::fmt::Display>(
-    t: &T,
-    serializer: S,
-) -> Result<S::Ok, S::Error> {
-    serializer.serialize_str(&t.to_string())
-}
-
-fn deserialize_from_string<'de, D: Deserializer<'de>, T: std::str::FromStr>(
-    deserializer: D,
-) -> Result<T, D::Error> {
-    let s = String::deserialize(deserializer)?;
-    s.parse::<T>()
-        .map_err(|_| serde::de::Error::custom("Parse from string failed"))
-}
 
 #[derive(Clone, Debug, PartialEq, Call, Encode)]
 pub struct RequestIssueCall<T: Issue> {
