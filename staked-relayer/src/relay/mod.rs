baa2ba6bbb5f236fb66b86a34e047879ee512355
Gregory Hill <gregorydhill@outlook.com>
2021-03-12 13:03:22 +0000

    refactor: match duplicate block relay error and log
    Signed-off-by: Gregory Hill <gregorydhill@outlook.com>

--- staked-relayer/src/relay/mod.rs
+++ staked-relayer/src/relay/mod.rs
@@ -8,10 +8,17 @@ pub use backing::Client as BitcoinClient;
 
 pub use issuing::Client as PolkaBtcClient;
 
-use crate::core::Runner;
-use log::error;
+use crate::core::{Error as CoreError, Runner};
+use log::{error, info};
 use runtime::PolkaBtcProvider;
+use runtime::{
+    substrate_subxt::{Error as SubxtError, ModuleError, RuntimeError as SubxtRuntimeError},
+    Error as RuntimeError,
+};
 use std::time::Duration;
+
+const BTC_RELAY_MODULE: &str = "BTCRelay";
+const DUPLICATE_BLOCK_ERROR: &str = "DuplicateBlock";
 
 pub async fn run_relayer(
     runner: Runner<Error, BitcoinClient, PolkaBtcClient>,
@@ -20,8 +27,19 @@ pub async fn run_relayer(
 ) {
     loop {
         super::utils::wait_until_registered(&provider, timeout).await;
-        if let Err(err) = runner.submit_next().await {
-            error!("Failed to submit_next: {}", err);
+        match runner.submit_next().await {
+            Ok(_) => (),
+            Err(CoreError::Issuing(Error::PolkaBtcError(RuntimeError::XtError(
+                SubxtError::Runtime(SubxtRuntimeError::Module(ModuleError {
+                    ref module,
+                    ref error,
+                })),
+            )))) if module == BTC_RELAY_MODULE && error == DUPLICATE_BLOCK_ERROR => {
+                info!("Attempted to submit block that already exists")
+            }
+            Err(err) => {
+                error!("Failed to submit_next: {}", err);
+            }
         }
     }
 }
