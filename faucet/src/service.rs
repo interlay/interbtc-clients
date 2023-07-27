41de51b161772ced96019b0ff19d58c7ad7c429a
nakul1010 <nakul.amate@gmail.com>
2023-07-27 20:03:47 +0530

    fix: add tracing crate and required methods

--- faucet/src/service.rs
+++ faucet/src/service.rs
@@ -1,5 +1,10 @@
 use futures::{future::Either, Future, FutureExt};
-pub use runtime::ShutdownSender;
+pub use runtime::{ShutdownReceiver, ShutdownSender};
+
+pub enum TerminationStatus<Res> {
+    Cancelled,
+    Completed(Res),
+}
 
 pub async fn on_shutdown(shutdown_tx: ShutdownSender, future2: impl Future) {
     let mut shutdown_rx = shutdown_tx.subscribe();
@@ -7,6 +12,22 @@ pub async fn on_shutdown(shutdown_tx: ShutdownSender, future2: impl Future) {
 
     let _ = future1.await;
     future2.await;
+}
+
+async fn run_cancelable<F, Res>(mut shutdown_rx: ShutdownReceiver, future2: F) -> TerminationStatus<Res>
+where
+    F: Future<Output = Res>,
+{
+    let future1 = shutdown_rx.recv().fuse();
+    let future2 = future2.fuse();
+
+    futures::pin_mut!(future1);
+    futures::pin_mut!(future2);
+
+    match futures::future::select(future1, future2).await {
+        Either::Left((_, _)) => TerminationStatus::Cancelled,
+        Either::Right((res, _)) => TerminationStatus::Completed(res),
+    }
 }
 
 pub async fn wait_or_shutdown<F, E>(shutdown_tx: ShutdownSender, future2: F) -> Result<(), E>
